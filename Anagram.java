065bcdaa3ac3ae4945f8b63848af95a8c798c504
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-04-12 16:10:33 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,3 +1,10 @@
+/**
+ * Given two strings a and b consisting of lowercase characters. 
+ * The task is to check whether two given strings are an anagram of each other or not. 
+ * An anagram of a string is another string that contains the same characters, 
+ * only the order of characters can be different. 
+ * For example, “act” and “tac” are an anagram of each other.
+ */
 import java.util.*;
 
 public class Anagram
@@ -11,12 +18,13 @@ public class Anagram
         String str2 = sc.nextLine();
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
+        // Converting string to array
         char tempArray1[] = str1.toCharArray();
         char tempArray2[] = str2.toCharArray();
+        // Sorting the strings
         java.util.Arrays.sort(tempArray1);
         java.util.Arrays.sort(tempArray2);
-
-        // return new sorted string
+        // Return Sorted string
         String str3 =  new String(tempArray1);
         String str4 =  new String(tempArray2);
         if(str3.equals(str4))
